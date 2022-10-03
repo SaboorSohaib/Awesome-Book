b77ef89d71298ea4e4a3c98862404cdc5a537bcd
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-03 14:50:41 +0430

    Add some comments

--- index.js
+++ index.js
@@ -48,21 +48,19 @@ form.addEventListener('submit', (e) => {
 window.onload = showBooks();
 
 // ======== NAVIGATIONS =================
-
 const contactLink = document.querySelector('.contact-us');
-
 const listOfBooks = document.querySelector('.container');
-
 const booksection = document.querySelector('.add-book');
 const listLink = document.querySelector('.list');
 const contactSection = document.querySelector('.contact');
 const addNewLink = document.querySelector('.add-new');
-
 const titleBookOne = document.querySelector('.books-title');
 
+// Luxon Date
 const showDate = document.querySelector('.our-date');
 showDate.innerHTML = DateTime.now();
 
+// Single page Part
 listLink.addEventListener('click', (e) => {
   e.preventDefault();
   titleBookOne.style.display = 'block';
